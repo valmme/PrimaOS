88025f4e814ba8c4c3f087926f149c0925231a95
vaqif <vaqif@DESKTOP-1L6F57R>
2024-11-10 21:15:01 +0400

    Add project files.

---
+++ PrimaOS/Kernel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Sys = Cosmos.System;
+using Cosmos.System.FileSystem.VFS;
+using Cosmos.System.FileSystem;
+
+
+namespace PrimaOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        PrimaOS prima = new PrimaOS();
+
+        string currentDir = @"0:/root";
+        string user = "user";
+        string pcname = "prima";
+
+        protected override void BeforeRun()
+        {
+
+            VFSManager.RegisterVFS(new CosmosVFS());
+            Console.Clear();
+        }
+
+        protected override void Run()
+        {
+            prima.ColorWrite($"{user}@{pcname}:", ConsoleColor.Black, ConsoleColor.Green);
+            prima.ColorWrite(currentDir, ConsoleColor.Black, ConsoleColor.Blue);
+
+            string command = prima.ReadLine($"$ ");
+
+            RunCommand(command);
+        }
+
+        public void RunCommand(string command)
+        {
+            if (command.StartsWith("echo "))
+            {
+                command = command.Substring(5);
+                Console.WriteLine(command);
+            }
+        }
+    }
+}
---
+++ PrimaOS/System.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace PrimaOS
+{
+    public class PrimaOS
+    {
+        public string VERSION = "0.0.2";
+        public string DATE = "11.7.2024";
+
+        public string ReadLine(string? text = null)
+        {
+            if (text != null) Console.Write(text);
+            return Console.ReadLine();
+        }
+
+        public void ColorWrite(string text, ConsoleColor bg, ConsoleColor fg)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bg;
+
+            Console.Write(text);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public void ColorWriteLine(string text, ConsoleColor bg, ConsoleColor fg)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bg;
+
+            Console.WriteLine(text);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public List<string> commands = new List<string> {
+            "mkdir [path] - create new directory",
+            "mk [filename] - create new file",
+            "rm [filename] - remove file",
+            "rmdir [path] - remove empty directory",
+            "cat [filename] - read file",
+            "lito [filename] - text editor for files",
+            "ls [path] - list of files and directories in current directory",
+            "clear - clear terminal",
+            "oxis [filename] - run OXIS file"
+        };
+    }
+}
